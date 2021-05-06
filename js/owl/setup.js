3eb2811dc1a7d09f0b75c3240cd554e064d3235e
felipeaguiarn <felipesaguiarn@gmail.com>
2021-05-05 23:08:14 -0300

    frist commit

---
+++ js/owl/setup.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:5
+        },
+        1400:{
+            items:6
+        }
+    }
+})
+
